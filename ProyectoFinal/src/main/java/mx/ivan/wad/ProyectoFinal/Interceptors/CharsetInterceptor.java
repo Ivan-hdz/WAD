39b82d8a8af32242a839d0993e4e22ab2aa17ee5
Ivan-Hdz <honter1997@gmail.com>
2019-06-01 03:05:10 -0500

    Ahora si sin errores

---
+++ ProyectoFinal/src/main/java/mx/ivan/wad/ProyectoFinal/Interceptors/CharsetInterceptor.java
@@ -0,0 +1,42 @@
+package mx.ivan.wad.ProyectoFinal.Interceptors;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+import com.opensymphony.xwork2.interceptor.Interceptor;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.struts2.StrutsStatics;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.logging.Logger;
+
+public class CharsetInterceptor extends AbstractInterceptor {
+ 
+
+    private static Logger logger = Logger.getLogger(CharsetInterceptor.class.getName());
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public String intercept(ActionInvocation invocation) throws Exception {
+        logger.info("Setting UTF-8");
+        ActionContext context = (ActionContext) invocation.getInvocationContext();
+        HttpServletRequest request = (HttpServletRequest) context.get(StrutsStatics.HTTP_REQUEST);
+        HttpServletResponse response=(HttpServletResponse) context.get(StrutsStatics.HTTP_RESPONSE);
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        return invocation.invoke();
+    }
+}
